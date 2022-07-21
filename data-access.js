21c7fbc676d7c4b8c26a8cbf48c41acde1044648
Kelsey Perkins <kperkins@xpanxion.com>
2022-07-21 14:59:45 -0700

    CHEECKS API

--- data-access.js
+++ data-access.js
@@ -22,7 +22,7 @@ const DELETE_TODOLIST_IN_TASK='delete from task where todo_list_id = $1'
 const DELETE_TODOLIST = 'delete from todo_list where todo_list_id = $1 ;'
 const CREATE_TODOLIST = 'INSERT INTO todo_list (todo_list_id,todo_list_name) VALUES ($1, $2) returning todo_list_id,todo_list_name;'
 const CREATE_TASK = 'INSERT INTO task (task_name, todo_list_id) VALUES ($1, $2) RETURNING task_id;'
-
+const PUT_UPDATESTATUS = 'update task set status_id = $1 where task_id = $2 returning status_id;'
 
 
 
@@ -101,4 +101,15 @@ module.exports.deleteTodoListInTask = async(todolistId) =>{
         console.error(err);
     }
     return retval;
+}
+
+module.exports.putUpdateTask = async (statusId,taskId) => {
+  let retval = null;
+  try {
+      let r = await pool.query(PUT_UPDATESTATUS, [statusId, taskId]);
+      retval = r.rows;
+  } catch (err) {
+      console.error(err);
+  }
+  return retval;
 }
